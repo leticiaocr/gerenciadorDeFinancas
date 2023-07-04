8f087e0e8a5d328a2c75a4a27b60d5ea79a6db75
leticiaocr <leticiabritooliveirac@gmail.com>
2023-07-04 07:15:29 -0300

    feature: inserindo dados no resumo financeiro

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@
 class Gerenciador {
     constructor() {
         this.formulario = document.querySelector('.insert');
+        this.amount = document.querySelector('.amount');
         this.event();
     }
 
@@ -16,15 +17,39 @@ class Gerenciador {
     // FUNÇÃO QUE USA O EVENTO DE SUBMIT PARA CAPTURAR OS VALORES DOS INPUTS
     handleSubmit(e) {
         e.preventDefault();
-        const description = this.formulario.querySelector('.description');
-        const value = this.formulario.querySelector('.value');
-        const type = this.formulario.querySelector('.type');
-        console.log(type.value);
+        const description = this.formulario.querySelector('.description').value;
+        const value = this.formulario.querySelector('.value').value;
+        const type = this.formulario.querySelector('.type').value;
+        this.enterData(description, value, type);
+        
     }
 
     // COLOCAR OS DADOS NO RESUMO FINANCEIRO 
+   
+    enterData(description, value, type) {
+       const local = document.querySelector('.list');
+       const descricao = document.createElement("p");
+       const valor = document.createElement("p");
+       const tipo = document.createElement("p");
 
-    // REALIZAR O CALCULO PARA DEFINIR SALDO ATUAL
+       descricao.textContent = description;
+       valor.textContent = value;
+       tipo.textContent = type; 
+
+       local.appendChild(descricao);
+       local.appendChild(valor);
+       local.appendChild(tipo);
+
+
+
+       // se o type for entrada adicionar seletor vermelho, se for saída adicionar verde
+
+    }
+
+
+    // REALIZAR O CALCULOS
+
+    // COLOCAR RESUMO DE ENTRADAS, SAÍDAS E TOTAL
 
     // TRABALHAR O LOCALSTORAGE
 }
